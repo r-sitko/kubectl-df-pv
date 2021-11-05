9214abd439dbc82c8e7d92d4a9329af4e025c0b4
rsitko92 <robert.sitko92@gmail.com>
2021-11-05 14:45:58 +0100

    Added app args parser and choose output format

---
+++ src/cli.rs
@@ -0,0 +1,18 @@
+use structopt::StructOpt;
+use strum::{EnumString, EnumVariantNames, VariantNames};
+
+const DEFAULT_FORMAT: &str = "table";
+
+#[derive(EnumString, EnumVariantNames, Debug)]
+#[strum(serialize_all = "kebab_case")]
+pub enum Format {
+    Table,
+    Json,
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "basic")]
+pub struct ApplicationArgs {
+    #[structopt(short, long, possible_values = Format::VARIANTS, case_insensitive = true, default_value = DEFAULT_FORMAT)]
+    pub format: Format,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,34 @@
+mod cli;
 mod models;
 mod output;
 mod stats;
 
+use crate::cli::{ApplicationArgs, Format};
 use crate::output::{JsonOutputFormatter, OutputGenerator, PrettyTableOutputFormatter};
 use crate::stats::PVStatsCollector;
+use structopt::StructOpt;
 
 // kubectl get --raw /api/v1/nodes/minikube/proxy/stats/summary
 // kubectl get --raw /api/v1/nodes/minikube-m02/proxy/stats/summary
 
 #[tokio::main]
 async fn main() -> Result<(), kube::Error> {
+    let app_args = ApplicationArgs::from_args();
+
     let pv_stats_collector = PVStatsCollector::new().await?;
     let pvs_stats = pv_stats_collector.get_pvs_stats().await?;
 
-    let output_generator = OutputGenerator::new(PrettyTableOutputFormatter, std::io::stdout());
-    output_generator.generate(&pvs_stats).unwrap();
+    match app_args.format {
+        Format::Table => {
+            let output_generator =
+                OutputGenerator::new(PrettyTableOutputFormatter, std::io::stdout());
+            output_generator.generate(&pvs_stats).unwrap();
+        }
+        Format::Json => {
+            let output_generator = OutputGenerator::new(JsonOutputFormatter, std::io::stdout());
+            output_generator.generate(&pvs_stats).unwrap();
+        }
+    }
 
     Ok(())
 }
