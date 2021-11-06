e779eb077d9d66c69ce74dd15235c37f86fe5085
rsitko92 <robert.sitko92@gmail.com>
2021-11-06 19:00:13 +0100

    DEFAULT_FORMAT as lazy_static

--- src/args.rs
+++ src/args.rs
@@ -1,9 +1,12 @@
 use structopt::StructOpt;
-use strum::{EnumString, EnumVariantNames, VariantNames};
+use strum::{EnumString, EnumVariantNames, VariantNames, IntoStaticStr};
+use lazy_static::lazy_static;
 
-const DEFAULT_FORMAT: &str = "table";
+lazy_static!{
+    static ref DEFAULT_FORMAT: &'static str = Format::Table.into();
+}
 
-#[derive(EnumString, EnumVariantNames, Debug)]
+#[derive(EnumString, EnumVariantNames, IntoStaticStr, Debug)]
 #[strum(serialize_all = "kebab_case")]
 pub enum Format {
     Table,
@@ -12,6 +15,6 @@ pub enum Format {
 
 #[derive(StructOpt, Debug)]
 pub struct ApplicationArgs {
-    #[structopt(short, long, possible_values = Format::VARIANTS, case_insensitive = true, default_value = DEFAULT_FORMAT)]
+    #[structopt(short, long, possible_values = Format::VARIANTS, case_insensitive = true, default_value = &DEFAULT_FORMAT)]
     pub format: Format,
 }
